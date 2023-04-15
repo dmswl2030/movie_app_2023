9df4230490509b7d7dc4c9fa4d453e5201271680
EUNJI <eunji9180@naver.com>
2023-04-15 19:35:20 +0900

    230415 movieApp finish

---
+++ src/routes/Detail.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
+  }
+  //render로 컴포넌트를 DOM에 부착한 후, Mount가 완료된 후 componentDidMount가 호출
+}
+
+export default Detail;
